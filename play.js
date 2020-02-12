a60df8a32ca4c6829716d388bc3dfeabd97be6ad
rahomohamed <rahojmohamed@gmail.com>
2020-02-12 19:38:12 +0000

    add play.js

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require('./client');
+
 console.log('Connecting ...');
  connect();
+
+ const setupInput = function(cb) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  process.stdin.on('data', (key) => {
+    cb(key);
+  });
+  return stdin;
+}
+const handleUserInput = function(data){
+   if (data === '\u0003') {
+   process.exit();
+  }
+}
+
+setupInput(handleUserInput);
 
