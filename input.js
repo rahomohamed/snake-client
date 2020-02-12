7b5969849e351396ac3c917a608854521147a5c9
rahomohamed <rahojmohamed@gmail.com>
2020-02-12 20:14:11 +0000

    add changes

--- input.js
+++ input.js
@@ -15,13 +15,17 @@ const handleUserInput = function(key){
    if (key === '\u0003') {
    process.exit();
   } if (key === 'w') {
-    console.log('up');
+    console.log('Move: up');
   } if (key === 'a') {
-    console.log('left');
+    console.log('Move: left');
   } if (key === 's') {
-    console.log('right');
+    console.log('Move: right');
   } if (key === 'd') {
-    console.log('down');
+    console.log('Move: down');
+  } if (key === 'r') {
+    conn.write('Say: LOL')
+  } if (key === 'r') {
+    conn.write('Say: hayyy')
   }
 } 
 
